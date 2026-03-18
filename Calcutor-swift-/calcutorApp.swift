7d2cd2e026f7fc9fdbf1349c0a9b1aa3a03c92e0
ruzgarcemturkmen <ruzgarcemturkmen@gmail.com>
2026-03-18 15:36:40 +0300

    Add files via upload

---
+++ Calcutor-swift-/calcutorApp.swift
@@ -0,0 +1,17 @@
+//
+//  calcutorApp.swift
+//  calcutor
+//
+//  Created by Rüzgar cem Türkmen on 5.02.2026.
+//
+
+import SwiftUI
+
+@main
+struct calcutorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
